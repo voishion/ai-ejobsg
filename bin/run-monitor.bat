38362c6aba82f96c8f5bf3613e131c0312fe15b2
李露 <lilu1@hydsoft.com>
2023-08-30 15:28:52 +0800

    项目初始化

---
+++ bin/run-monitor.bat
@@ -0,0 +1,14 @@
+@echo off
+echo.
+echo [��Ϣ] ʹ��Jar��������Monitor���̡�
+echo.
+
+cd %~dp0
+cd ../ejobsg-visual/ejobsg-monitor/target
+
+set JAVA_OPTS=-Xms512m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m
+
+java -Dfile.encoding=utf-8 %JAVA_OPTS% -jar ejobsg-visual-monitor.jar
+
+cd bin
+pause
