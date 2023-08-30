3bc5a8b9877679ce5972e8aabafc55c5e674fe32
李露 <lilu1@hydsoft.com>
2023-08-30 15:28:40 +0800

    项目初始化

---
+++ ejobsg-ui/src/components/Ejobsg/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="question" @click="goto" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EjobsgDoc',
+  data() {
+    return {
+      url: 'http://doc.ejobsg.vip/ejobsg-cloud'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
