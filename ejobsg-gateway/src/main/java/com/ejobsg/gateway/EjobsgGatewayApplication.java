ffbd4ad995ff00572ffabf89407f1fb010441e1d
李露 <lilu1@hydsoft.com>
2023-08-31 09:30:34 +0800

    名称优化

--- ejobsg-gateway/src/main/java/com/ejobsg/gateway/EjobsgGatewayApplication.java
+++ ejobsg-gateway/src/main/java/com/ejobsg/gateway/EjobsgGatewayApplication.java
@@ -14,7 +14,7 @@ public class EjobsgGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EjobsgGatewayApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  若依网关启动成功   ლ(´ڡ`ლ)ﾞ");
+        System.out.println("(♥◠‿◠)ﾉﾞ  系统网关启动成功   ლ(´ڡ`ლ)ﾞ");
     }
 
 }
