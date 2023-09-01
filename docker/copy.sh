f7e6120103dcc7b0990b3c0fdb16d5b63b513dc6
李露 <lilu1@hydsoft.com>
2023-09-01 15:10:43 +0800

    tomcat转undertow，dockerfile优化

--- docker/copy.sh
+++ docker/copy.sh
@@ -9,13 +9,13 @@ usage() {
 
 # copy sql
 echo "begin copy sql "
-cp ../sql/ai-ejobsg-20230706.sql ./mysql/db
-cp ../sql/ai-ejobsg-config-20220929.sql ./mysql/db
-cp ../sql/ai-ejobsg-seata-20210128.sql ./mysql/db
+#cp ../sql/ai-ejobsg-20230706.sql ./mysql/db
+#cp ../sql/ai-ejobsg-config-20220929.sql ./mysql/db
+#cp ../sql/ai-ejobsg-seata-20210128.sql ./mysql/db
 
 # copy html
 echo "begin copy html "
-cp -r ../ejobsg-ui/dist/** ./nginx/html/dist
+#cp -r ../ejobsg-ui/dist/** ./nginx/html/dist
 
 
 # copy jar
@@ -29,14 +29,14 @@ echo "begin copy ejobsg-visual "
 cp ../ejobsg-visual/ejobsg-visual-monitor/target/ejobsg-visual-monitor.jar  ./ejobsg/visual/monitor/jar
 
 echo "begin copy ejobsg-modules-system "
-cp ../ejobsg-modules/ejobsg-system/target/ejobsg-modules-system.jar ./ejobsg/modules/system/jar
+cp ../ejobsg-modules/ejobsg-modules-system/target/ejobsg-modules-system.jar ./ejobsg/modules/system/jar
 
 echo "begin copy ejobsg-modules-file "
-cp ../ejobsg-modules/ejobsg-file/target/ejobsg-modules-file.jar ./ejobsg/modules/file/jar
+cp ../ejobsg-modules/ejobsg-modules-file/target/ejobsg-modules-file.jar ./ejobsg/modules/file/jar
 
 echo "begin copy ejobsg-modules-job "
-cp ../ejobsg-modules/ejobsg-job/target/ejobsg-modules-job.jar ./ejobsg/modules/job/jar
+cp ../ejobsg-modules/ejobsg-modules-job/target/ejobsg-modules-job.jar ./ejobsg/modules/job/jar
 
 echo "begin copy ejobsg-modules-gen "
-cp ../ejobsg-modules/ejobsg-gen/target/ejobsg-modules-gen.jar ./ejobsg/modules/gen/jar
+cp ../ejobsg-modules/ejobsg-modules-gen/target/ejobsg-modules-gen.jar ./ejobsg/modules/gen/jar
 
