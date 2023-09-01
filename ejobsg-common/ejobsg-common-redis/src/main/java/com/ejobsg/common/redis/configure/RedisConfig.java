a51a5b84fe796922f7ba4320c3bdbd3f5384e441
李露 <lilu1@hydsoft.com>
2023-09-01 15:32:57 +0800

    集成redisson实现redis分布式锁

--- ejobsg-common/ejobsg-common-redis/src/main/java/com/ejobsg/common/redis/configure/RedisConfig.java
+++ ejobsg-common/ejobsg-common-redis/src/main/java/com/ejobsg/common/redis/configure/RedisConfig.java
@@ -18,12 +18,11 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 @EnableCaching
 @AutoConfigureBefore(RedisAutoConfiguration.class)
-public class RedisConfig extends CachingConfigurerSupport
-{
+public class RedisConfig extends CachingConfigurerSupport {
+
     @Bean
-    @SuppressWarnings(value = { "unchecked", "rawtypes" })
-    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory)
-    {
+    @SuppressWarnings(value = {"unchecked", "rawtypes"})
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<Object, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
@@ -40,4 +39,5 @@ public class RedisConfig extends CachingConfigurerSupport
         template.afterPropertiesSet();
         return template;
     }
+
 }
