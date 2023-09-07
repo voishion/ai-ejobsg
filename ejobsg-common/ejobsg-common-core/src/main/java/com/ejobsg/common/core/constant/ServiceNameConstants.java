75ef17b132c66c93feb5233b4710f03b4871c175
李露 <lilu1@hydsoft.com>
2023-09-07 14:33:22 +0800

    集成Skywalking日志收集模块

--- ejobsg-common/ejobsg-common-core/src/main/java/com/ejobsg/common/core/constant/ServiceNameConstants.java
+++ ejobsg-common/ejobsg-common-core/src/main/java/com/ejobsg/common/core/constant/ServiceNameConstants.java
@@ -15,10 +15,10 @@ public class ServiceNameConstants
     /**
      * 系统模块的serviceid
      */
-    public static final String SYSTEM_SERVICE = "ejobsg-system";
+    public static final String SYSTEM_SERVICE = "ejobsg-modules-system";
 
     /**
      * 文件服务的serviceid
      */
-    public static final String FILE_SERVICE = "ejobsg-file";
+    public static final String FILE_SERVICE = "ejobsg-modules-file";
 }
