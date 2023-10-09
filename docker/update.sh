ea3ff0895f3f012d10767df9e968f714584d3477
李露 <lilu1@hydsoft.com>
2023-10-09 16:00:19 +0800

    更新命令优化

--- docker/update.sh
+++ docker/update.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 usage() {
 	echo "Usage: sh update.sh"
@@ -8,9 +8,14 @@ read -p "Enter need update container name: " name
 containerName=$name
 echo "Start update $containerName container"
 
+# 关闭应用
 docker stop $containerName
-docker rm -f $containerName
+# 删除容器
+docker container rm $containerName
+# 删除镜像
 docker images | grep $containerName | awk '{print $3}' | xargs docker rmi
+
+# 重新构建部署
 docker-compose up -d $containerName
 
 echo "Update $containerName container success"
